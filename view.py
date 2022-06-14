b27e9a05ecd33db7138da418b0aeae718f707baa
Nununk <hek18@qq.com>
2022-06-15 06:21:18 +0800

    创建视图login

--- view.py
+++ view.py
@@ -1,2 +1,7 @@
-for 
-end for
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def index(request)
+	return HttpRespose('index')
+def login(request)
+	return redirect('/index')
